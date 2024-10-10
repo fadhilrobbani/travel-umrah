d0f7f44aed35fdc0d63e2cdf0fe1d7f18e83ac69
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-10-10 23:02:01 +0700

    update export

--- app/Filament/Resources/JamaahResource/Pages/ListJamaahs.php
+++ app/Filament/Resources/JamaahResource/Pages/ListJamaahs.php
@@ -20,8 +20,33 @@ class ListJamaahs extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
-            PagesExportAction::make('export')
-
+            PagesExportAction::make()->exports([
+                ExcelExport::make()->withColumns([
+                    Column::make('id'),
+                    Column::make('nama'),
+                    Column::make('nik')->formatStateUsing(fn($state) => strval($state)),
+                    Column::make('alamat'),
+                    Column::make('provinsi')->formatStateUsing(fn($state) => \Indonesia::findProvince($state)?->name ?? $state),
+                    Column::make('kota')->formatStateUsing(fn($state) => \Indonesia::findCity($state)?->name ?? $state),
+                    Column::make('kecamatan')->formatStateUsing(fn($state) => \Indonesia::findDistrict($state)?->name ?? $state),
+                    Column::make('kelurahan')->formatStateUsing(fn($state) => \Indonesia::findVillage($state)?->name ?? $state),
+                    Column::make('tempat_lahir'),
+                    Column::make('tanggal_lahir'),
+                    Column::make('jenis_kelamin'),
+                    Column::make('no_paspor'),
+                    Column::make('masa_berlaku_paspor'),
+                    Column::make('no_visa'),
+                    Column::make('masa_berlaku_visa'),
+                    Column::make('ktp'),
+                    Column::make('kk'),
+                    Column::make('foto'),
+                    Column::make('paspor'),
+                    Column::make('paket'),
+                    Column::make('kamar'),
+                    Column::make('created_at'),
+                    Column::make('updated_at'),
+                ])
+            ])
         ];
     }
 }
