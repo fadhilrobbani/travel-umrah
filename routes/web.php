0ea331bb2278dcd674aebb76ac592a4546578934
fadhilrobbani <rabbanifadhillailham@gmail.com>
2024-10-10 21:16:20 +0700

    disable test

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/', function () {
     return redirect('/admin');
 });
 
-Route::get('/test', function () {
-    return view('test');
-});
+// Route::get('/test', function () {
+//     return view('test');
+// });
